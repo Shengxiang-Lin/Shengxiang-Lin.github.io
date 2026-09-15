69f1f0754a7abe0146be920404309a3d50e2c2e9
Shengxiang Lin <154602793+Shengxiang-Lin@users.noreply.github.com>
2026-09-15 09:36:10 +0800

    Reorder homepage sections
    * Add standalone education card
    
    * Add standalone experience card
    
    * Add standalone news card
    
    * Add standalone awards card
    
    * Add standalone service card
    
    * Reorder homepage sections
    
    * Move collapsible section behavior to common JS
    
    * Version common script after homepage refactor

--- assets/js/common.js
+++ assets/js/common.js
@@ -11,3 +11,23 @@ $(function () {
     })
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('[data-collapsible-toggle]').forEach(function (button) {
+        button.addEventListener('click', function () {
+            var target = button.getAttribute('data-collapsible-toggle');
+            var list = document.querySelector('[data-collapsible-list="' + target + '"]');
+            if (!list) return;
+
+            var expanded = button.getAttribute('aria-expanded') === 'true';
+            list.querySelectorAll('[data-collapsible-extra]').forEach(function (item) {
+                item.classList.toggle('d-none', expanded);
+            });
+            list.classList.toggle('is-expanded', !expanded);
+
+            button.setAttribute('aria-expanded', String(!expanded));
+            button.querySelector('[data-collapsible-more]').classList.toggle('d-none', !expanded);
+            button.querySelector('[data-collapsible-less]').classList.toggle('d-none', expanded);
+        });
+    });
+});
